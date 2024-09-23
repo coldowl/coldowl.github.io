df73bab99c10bec3a82a8714bd6608ddccb0e2a1
coldowl <coldowl@163.com>
2024-09-23 03:04:25 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [3](https://coldowl.github.io/tag.html) 
 ### :speech_balloon: 0 
 ### :hibiscus: 10106 
-### :alarm_clock: 2024-09-23 11:02:57 
+### :alarm_clock: 2024-09-23 11:04:25 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
---
+++ docs/EasterEgg.js
@@ -0,0 +1,48 @@
+function loadResource(type, attributes) {
+  if (type === 'style') {
+    const style = document.createElement('style');
+    style.textContent = attributes.css;
+    document.head.appendChild(style);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const css = `#hidden-content {
+    display: none;
+    opacity: 0;
+    transition: opacity 1s ease;
+}
+
+#hidden-content.show {
+    display: block;
+    opacity: 1;
+}
+`;
+  loadResource('style', { css: css });
+  
+});
+
+// 预设的触发密码
+const secretCode = 'open';
+// 保存用户输入的字符
+let userInput = [];
+
+document.addEventListener('keydown', function(event) {
+    userInput.push(event.key);
+
+    if (userInput.length > secretCode.length) {
+        userInput.shift();
+    }
+
+    if (userInput.join('').toLowerCase() === secretCode.slice(0, 3)) {
+        document.getElementById('hint-message').style.display = 'block';
+    }
+
+    if (userInput.join('').toLowerCase() === secretCode) {
+        const hiddenContent = document.getElementById('hidden-content');
+        hiddenContent.classList.add('show');
+
+        // 禁用进一步的监听器
+        document.removeEventListener('keydown', arguments.callee);
+    }
+});
